0ad6fd09102523361fd21fbf63e259798a03b536
Batcha <81833517+BacaR00T@users.noreply.github.com>
2024-01-19 19:51:07 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,10 +5,17 @@ from kmk.kmk_keyboard import KMKKeyboard
 from kmk.keys import KC
 from kmk.scanners import DiodeOrientation
 from kmk.modules.layers import Layers
+from kmk.handlers.sequences import simple_key_sequence
+from kmk.modules.holdtap import HoldTap
+from kmk.modules.tapdance import TapDance
+from kmk.extensions.media_keys import MediaKeys
+
 
 keyboard = KMKKeyboard()
-layers_ext = Layers()
-                    #LEFT              MMVVEE      VVVVCXSS  CCCC FFRV                               #RIGHT
+keyboard.extensions.append(MediaKeys())
+keyboard.modules = [HoldTap(), Layers(), TapDance()]
+
+                    #LEFT                                           #RIGHT
 keyboard.col_pins = (board.GP10, board.GP11, board.GP12, board.GP13, board.GP14, board.GP21, board.GP20,board.GP19,board.GP18,board.GP17) # Cols
 keyboard.row_pins = (board.GP5, board.GP4, board.GP3,board.GP2 ) # Rows
 keyboard.diode_orientation = DiodeOrientation.COL2ROW
@@ -18,16 +25,12 @@ XXXXXXX = KC.NO
 
 LOWER = KC.MO(1)
 RAISE = KC.MO(2)
-
-#from kmk.extensions.RGB import RGB
-#rgb = RGB(
-#    pixel_pin=board.GP27, 
-#    num_pixels=2, 
-#    hue_default =100, 
-#    sat_default=255, 
-#    val_default=10,	
-#    )
-#keyboard.extensions.append(rgb)
+SPCLOW = KC.HT(KC.SPC, LOWER)
+ENTRAI = KC.HT(KC.ENT, RAISE)
+ESCALT = KC.HT(KC.ESC, KC.LALT)
+TABCTRL = KC.HT(KC.TAB, KC.LCTRL)
+SUPERSHIFT = KC.HT(KC.LGUI, KC.LSHIFT)
+BSPDEL = KC.HT(KC.BSPC, KC.DEL)
 
 # Keymap
 keyboard.keymap = [
@@ -36,20 +39,20 @@ keyboard.keymap = [
         KC.Q,   KC.W,   KC.E,   KC.R,   KC.T,       KC.Y,   KC.U,   KC.I,   KC.O,   KC.P,
         KC.A,   KC.S,   KC.D,   KC.F,   KC.G,       KC.H,   KC.J,   KC.K,   KC.L,   KC.SCOLON,
         KC.Z,   KC.X,   KC.C,   KC.V,   KC.B,       KC.N,   KC.M,   KC.COMMA, KC.DOT , KC.SLASH,
-        XXXXXXX,XXXXXXX,RAISE, KC.BSPC, KC.SPC,     KC.ENT, KC.LCTRL, LOWER, XXXXXXX, XXXXXXX,
+        XXXXXXX,XXXXXXX,ESCALT,BSPDEL, SPCLOW,     ENTRAI,SUPERSHIFT,TABCTRL, XXXXXXX, XXXXXXX,
     ],
     #LOWER
     [   
         KC.TAB,    KC.N1,   KC.N2,   KC.N3,   KC.N4,       KC.F1,   KC.F2,   KC.F3,   KC.F4,   KC.F5,
         KC.CAPS,     KC.N5,   KC.N6,   KC.N7,   KC.N8,       KC.F6,   KC.F7,   KC.F8,   KC.F9,   KC.F10,
         KC.LSHIFT,    KC.N9,   KC.N0,   KC.MINUS,   KC.EQL,   KC.F11,   KC.F12,   KC.QUOT, KC.LBRC , KC.RBRC,
-        XXXXXXX,XXXXXXX,RAISE, KC.BSC, KC.SPC,              KC.ENT, KC.ESC, LOWER, XXXXXXX, XXXXXXX,
+        XXXXXXX,XXXXXXX,KC.TAB, KC.BSC, KC.SPC,              KC.ENT, KC.ESC, LOWER, XXXXXXX, XXXXXXX,
     ],
     #RAISE/MOUSE
     [  
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        KC.TAB,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,            XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC.MW_UP,	        KC.LEFT,   KC.UP,   KC.DOWN,   KC.RGHT, XXXXXXX,
-        XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,KC.MW_DN,           KC.MB_MMB,   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
+        XXXXXXX,XXXXXXX,KC.LALT,XXXXXXX,KC.MW_DN,           KC.MB_MMB,   XXXXXXX,XXXXXXX,XXXXXXX,XXXXXXX,
         XXXXXXX,XXXXXXX,RAISE, KC.BSPACE, KC.LGUI,        KC.ENT, KC.ESC, LOWER, XXXXXXX, XXXXXXX,
     ],
 
